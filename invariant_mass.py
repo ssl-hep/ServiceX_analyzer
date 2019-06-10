b43a29312f23545309fbb5b6c9ca4f98c922c659
Ben Galewsky <bengal1@illinois.ed>
2019-06-10 14:30:59 -0700

    Switch to confluent kafka lib and increase inital timeout

--- invariant_mass.py
+++ invariant_mass.py
@@ -2,8 +2,11 @@
 import awkward
 import pyarrow as pa
 import argparse
-from kafka import KafkaConsumer
+
+import sys
+from confluent_kafka import Consumer, KafkaException
 import uproot_methods
+import logging
 
 kakfa_brokers = []
 
@@ -20,33 +23,59 @@ parser.add_argument("--topic", dest='topic', action='store',
 
 args = parser.parse_args()
 
-try:
-    consumer = KafkaConsumer(args.topic, auto_offset_reset='earliest',
-                             bootstrap_servers=[args.broker],
-                             api_version=(0, 10), consumer_timeout_ms=1000,
-                             group_id=None)
+# Create logger for consumer (logs will be emitted when poll() is called)
+logger = logging.getLogger('consumer')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setFormatter(
+    logging.Formatter('%(asctime)-15s %(levelname)-8s %(message)s'))
+logger.addHandler(handler)
 
+try:
+    conf = {'bootstrap.servers': args.broker, 'group.id': 'inv',
+            'session.timeout.ms': 6000,
+            'auto.offset.reset': 'earliest'}
+    c = Consumer(conf, logger=logger)
+
+
+    def print_assignment(consumer, partitions):
+        print('Assignment:', partitions)
+
+
+    # Subscribe to topics
+    c.subscribe([args.topic], on_assign=print_assignment)
+
+    timeout = 55.0 # Need a long timeout to allow for partition assignment
     running = True
     while running:
-        raw_messages = consumer.poll(timeout_ms=1000, max_records=5000)
-        if raw_messages != {}:
-            for topic_partition, messages in raw_messages.items():
-                for msg in messages:
-                    buf = msg.value
-                    reader = pa.ipc.open_stream(buf)
-                    batches = [b for b in reader]
-                    for batch in batches:
-                        arrays = awkward.fromarrow(batch)
-                        v_particles = uproot_methods.TLorentzVectorArray.from_ptetaphi(
-                            arrays['Electrons_pt'], arrays['Electrons_eta'],
-                            arrays['Electrons_phi'], arrays['Electrons_e'],
-                        )
-                        v_particles = v_particles[v_particles.counts >= 2]
-                        diparticles = v_particles[:, 0] + v_particles[:, 1]
-                        print("Diparticle mass: " + str(diparticles.mass))
-        else:
+        msg = c.poll(timeout=timeout)
+        if msg is None:
             running = False
+            continue
+        if msg.error():
+            raise KafkaException(msg.error())
+        else:
+            # Proper message
+            sys.stderr.write('%% %s [%d] at offset %d with key %s:\n' %
+                             (msg.topic(), msg.partition(), msg.offset(),
+                              str(msg.key())))
 
+            buf = msg.value()
+            reader = pa.ipc.open_stream(buf)
+            batches = [b for b in reader]
+            for batch in batches:
+                arrays = awkward.fromarrow(batch)
+                v_particles = uproot_methods.TLorentzVectorArray.from_ptetaphi(
+                    arrays['Electrons_pt'], arrays['Electrons_eta'],
+                    arrays['Electrons_phi'], arrays['Electrons_e'],
+                )
+                v_particles = v_particles[v_particles.counts >= 2]
+                diparticles = v_particles[:, 0] + v_particles[:, 1]
+                print("Diparticle mass: " + str(diparticles.mass))
+
+                # Once we are assigned a partition and start getting messages
+                # we can tighten up the timeout
+                timeout = 10.0
 except Exception as ex:
     print(ex)
     raise
